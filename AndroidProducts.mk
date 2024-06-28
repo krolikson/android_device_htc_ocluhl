1b80d3718d37549668764fc19f404d22196b2a81
krolikson <46311864+krolikson@users.noreply.github.com>
2024-06-28 21:05:14 +0100

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_htc_ocluhl.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_htc_ocluhl-user \
+    omni_htc_ocluhl-userdebug \
+    omni_htc_ocluhl-eng
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,8 @@
-# Android device tree for htc HTC U11 life (htc_ocluhl)
-
-```
 #
 # Copyright (C) 2024 The Android Open Source Project
 # Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-```
+
+
